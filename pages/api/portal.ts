c8aadee268e614fcf1c1c4250fff9310b09aa1b9
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-02-07 02:37:19 +0530

    ability to upload files

--- pages/api/portal.ts
+++ pages/api/portal.ts
@@ -1,9 +1,28 @@
-export default function handler(req:any, res:any) {
- 
-  const body = req.body
+import formidable from 'formidable';
+import path from 'path';
+import fs from 'fs';
 
-  
-  console.log('body: ', body)
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
 
-  //
+const saveFile = async (file: any) => {
+  const data = fs.readFileSync(file.path);
+  fs.writeFileSync(`./public/${file.name}`, data);
+  await fs.unlinkSync(file.path);
+  return;
+};
+
+export default async function handler(req: any, res: any) {
+  const form = formidable({
+    uploadDir: path.join(process.cwd(), '/public'),
+    filename: (name, ext, path, form) => {
+      return Date.now().toString() + path.originalFilename;
+    },
+  });
+  form.parse(req, async (errors, fields, files) => {
+    res.json({ status: 200 });
+  });
 }
--- pages/portal.tsx
+++ pages/portal.tsx
@@ -1,42 +1,58 @@
-import type { NextPage } from "next";
+import type { NextPage } from 'next';
+import { useState } from 'react';
 
 const Portal: NextPage = () => {
-    const handleSubmit = async (event: any) => {
-        event.preventDefault();
+  const [first, setFirst] = useState('');
+  const [last, setLast] = useState('');
+  const [file, setFile] = useState<File | null>(null);
 
-        console.log(event.target.file.value);
-        const data = {
-            first: event.target.first.value,
-            last: event.target.last.value,
-        };
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append('first', first);
+    formData.append('last', last);
 
-        // const JSONdata = JSON.stringify(data);
-        //const sentData = 
-        const endpoint = "/api/portal";
-
-        const options = {
-            method: "POST",
-
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: event.target.file.value,
-        };
-
-        const response = await fetch(endpoint, options);
-
-        const result = await response.json();
+    if (file) {
+      formData.append('file', file);
+    }
+    const options = {
+      method: 'POST',
+      body: formData,
     };
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="first">First Name</label>
-            <input type="text" id="first" name="first" required />
+    const response = await fetch('/api/portal', options);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='first'>First Name</label>
+      <input
+        onChange={(e) => setFirst(e.target.value)}
+        type='text'
+        id='first'
+        name='first'
+        required
+      />
 
-            <label htmlFor="last">Last Name</label>
-            <input type="text" id="last" name="last" required />
-            <input type="file" id="file" name="file" />
-            <button type="submit">Submit</button>
-        </form>
-    );
+      <label htmlFor='last'>Last Name</label>
+      <input
+        onChange={(e) => setLast(e.target.value)}
+        type='text'
+        id='last'
+        name='last'
+        required
+      />
+
+      <input
+        onChange={(e) => {
+          if (e.target.files && e.target.files[0]) {
+            setFile(e.target.files[0]);
+          }
+        }}
+        type='file'
+        id='file'
+        name='file'
+      />
+      <button type='submit'>Submit</button>
+    </form>
+  );
 };
 export default Portal;
